3e73d33992154af58800e72860d73192d1e34682
stefano <szevallosl@hotmail.com>
2023-06-16 11:16:47 -0500

    agregue la rama desarrollo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className="bg-gray-300 h-screen w-screen">
+      <header className="bg-slate-700 h-24 w-full flex justify-start "> 
+      <Image className='pl-4 mt-4 h-14 w-32' src="/GameOnLogo.png"  width={100} height={100} alt="Logo de GameOn"/>
+      </header>
+      <div className='flex justify-center absolute w-full top-20'>
+        <input className='w-200 h-10 rounded-full pl-5' type="text" placeholder='Ingrese su búsqueda' /> 
+      </div>
+    </div>
   )
 }
 
 export default page
+
+
+
+
