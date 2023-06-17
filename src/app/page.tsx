353a8d994e8eef9cc49e24faf1ee602783151115
stefano <szevallosl@hotmail.com>
2023-06-16 22:16:46 -0500

    hotfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Footer from '@/components/Footer'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 
 const page = () => {
   return (
     <>
+    <Header></Header>
     <Footer></Footer>
     </>
   )
