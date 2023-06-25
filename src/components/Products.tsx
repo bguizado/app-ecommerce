0a7aa00b6757d9961b4ec0cd739922504dd874ce
bguizado <brando.guizado.87@gmail.com>
2023-06-25 01:03:33 -0500

    Instalando librería de Slider

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,15 +1,31 @@
+'use client'
+
 import React from "react";
 import { getProducts } from "../api/api";
 import { IProducts } from "@/models/product";
 import { Cards } from "@/components/Cards";
+import Slider from "react-slick";
+import '../../node_modules/slick-carousel/slick/slick.css';
+import '../../node_modules/slick-carousel/slick/slick-theme.css';
+
+
 
 export async function Products() {
-  const products = await getProducts();
+  
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
 
+  const products = await getProducts();
   return (
     <section className="bg-white">
       <div className="mx-auto max-w-7xl overflow-hidden sm:px-6 lg:px-8">
         <h2 className="mt-10">Productos en oferta</h2>
+        {/* <Slider {...settings}> */}
         <div className="-mx-px grid grid-cols-2 sm:mx-0 md:grid-cols-3 lg:grid-cols-4">
           {products
             .filter(
@@ -21,6 +37,7 @@ export async function Products() {
               </div>
             ))}
         </div>
+        {/* </Slider> */}
         <h2 className="mt-10">Productos recomendados</h2>
         <div className="-mx-px grid grid-cols-2 sm:mx-0 md:grid-cols-3 lg:grid-cols-4">
           {products
