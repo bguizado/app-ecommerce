e25bc131077c64ba5576f43ce188d6fd2ca80394
Julio Teran <juliocesarteraninares55mail.com>
2023-06-25 12:58:33 -0500

    1-productoestatico

---
+++ src/components/Carritocompras.tsx
@@ -0,0 +1,28 @@
+
+function Carritocompras() {
+  return (
+
+    <div className="flex flex-shrink-0 w-108 h-28 border-b-2 border-b-gray-300 justify-around items-center"> 
+     <img src="https://i.ibb.co/j3P2pJf/167115045487122.jpg" alt="" className="w-28 h-24 items-center"/>
+      <div className="">
+        <h2 className="text-lg tex-neutral-500 justify-center w-35 h-7 flex-nowrap font-bold">Monitor Benq EL2870U</h2>
+        <p className="tex-sm w-30 h-8"> S./ 2000.30</p>
+      </div>
+      <div className="">
+        <button className="text-2xl w-3.5 h-9 text-center font-normal">
+        +
+        </button> <hr />
+        <p className="text-lg font-normal text-center">
+          1
+        </p> <hr />
+        <button className="text-2xl w-3 h-9 text-center font-normal">
+          -
+        </button>
+      </div>
+    </div>
+    
+  )
+}
+
+
+export default Carritocompras
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,9 @@ const Header = () => {
         <div className="bg-gray-300 "> {/* mb-10 h-1/4 */}
             <header className="bg-slate-700 h-24 w-full flex justify-start relative ">
                 <Image className='pl-4 mt-4 h-14 w-32' src="/logo.svg" width={100} height={100} alt="Logo de GameOn" />
-                <div className='flex justify-center absolute w-full top-20'>
+                {/* <div className='flex justify-center absolute w-full top-20'>
                     <input className='w-200 h-10 rounded-full pl-5' type="text" placeholder='Ingrese su búsqueda' />
-                </div>
+                </div> */}
             </header>
         </div>
     )
