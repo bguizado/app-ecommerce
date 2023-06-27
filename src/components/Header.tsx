e9ccfe7d6de366b0bff65927f072a18a7dd078e2
stefano <szevallosl@hotmail.com>
2023-06-27 12:26:32 -0500

    cambios a header mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
           />
         </div>
         <div className="w-40 h-24 flex justify-end space-x-3 mr-2 items-center lg:w-full 2xl:space-x-3 2xl:mr-5">
-        <div className="2xl:flex 2xl:items-center 2xl:space-x-1">
+        <div className="2xl:flex 2xl:items-center 2xl:space-x-1 mt-2">
             <p className="hidden 2xl:block 2xl:text-orange-300 2xl:font-semibold 2xl:cursor-pointer 2xl:text-md">Iniciar Sesión</p>
             <Image
               className="h-8 w-8 cursor-pointer 2xl:h-10 2xl:w-10 "
@@ -25,7 +25,7 @@ const Header = () => {
               alt="Usuario"
             />
           </div>
-        <div className="2xl:flex 2xl:items-center 2xl:space-x-1.5">
+        <div className="2xl:flex 2xl:items-center 2xl:space-x-1.5 mt-2">
             <p className="hidden 2xl:block 2xl:text-orange-300 2xl:font-semibold 2xl:cursor-pointer 2xl:text-md " >Carrito</p>
             <Image
             className="h-6 w-6 2xl:h-8 2xl:w-8 cursor-pointer"
