e480065bdd81e63fede8f41cc3f77788004d1461
Julio Teran <juliocesarteraninares55mail.com>
2023-06-27 19:58:02 -0500

    diseño estatico casi terminado falta api,etc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Carritocompras from "../components/Carritocompras"
+import {getProducts} from "../api/api"
+import Preciocarrito from "@/components/preciocarrito";
+
 const Home = () => {
   return (
     <div className='p-0 w-screen h-4/5'>
       {/* ver si se puede aplicar flex-1 al contenido y al body colocarle flex  y quitar ese p-*/}
-      { <Carritocompras/> }
-      { <Carritocompras/> }
-      { <Carritocompras/> }
+      { <Preciocarrito/>}
+      
+       
       
 
     </div>
--- src/components/Carritocompras.tsx
+++ src/components/Carritocompras.tsx
@@ -1,25 +1,36 @@
 "use client"
-import { useState } from "react"
-
+import { useState, useEffect } from "react"
 
 
 function Carritocompras() {
+  
+
+  
   const [numpro , setnumpro] = useState(1)
   return (
 
-    <div className="flex flex-shrink-0 w-108 h-28 border-b-2 border-b-gray-300 justify-around items-center"> 
-     <img src="https://i.ibb.co/j3P2pJf/167115045487122.jpg" alt="" className="w-28 h-24 items-center"/>
+    <div className="flex flex-shrink-0  border-b-2 border-b-gray-300 justify-around items-center "> 
+     <img src="https://i.ibb.co/1dhTSKZ/167520951358224.jpg"
+       alt="" className="w-28 h-24 items-center"/>
       <div className="">
-        <h2 className="text-lg tex-neutral-500 justify-center w-35 h-7 flex-nowrap font-bold">Monitor Benq EL2870U</h2>
+        <h2 className="text-lg tex-neutral-500 justify-center w-35 text-[#99A4AC] flex-nowrap font-bold">Monitor Benq EL2870U</h2>
         <p className="tex-sm w-30 h-8"> S./ 2000.30</p>
       </div>
-      <div className="flex-col">
-        <button className="text-2xl w-3.5 h-9 text-center font-normal" onClick={() => { setnumpro (numpro +1)}} >
+      
+      <div className="flex-col items-baseline">
+        <button className="text-2xl w-3.5 h-9 b-2 text-center font-normal " onClick={() => { setnumpro (numpro +1)}} >
         +
-        </button> <hr />
-        <p className="text-lg font-normal text-center">
+        </button> 
+        <div className="flex">
+          {/*en el parrafo modificar tamaño de letra recomendado 0.925rem*/}
+        <p className="text-lg font-normal text-center w-4 mr-1">
           {numpro}
-        </p> <hr />
+        </p> 
+        <button className="ml-4" onClick={()=> { alert("El producto ha sido eliminado")}}> <svg className="h-6 w-6 text-red-400"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+</svg>
+</button>
+        </div> 
         <button className="text-2xl w-3 h-9 text-center font-normal" onClick={() => setnumpro( numpro -1 )}>
           -
         </button>
