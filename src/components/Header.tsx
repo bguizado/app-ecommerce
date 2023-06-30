5750d65fd91d86644cc907c811d1fb49b76076a4
stefano <szevallosl@hotmail.com>
2023-06-30 16:09:07 -0500

    arreglando el Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,57 @@
 import React from "react";
 import Image from "next/image";
+import Buscador from "./Buscador";
 
 const Header = () => {
   return (
     <>
-      <div className="bg-slate-700 h-24 w-full flex items-center  ">
-        <div className="pl-4 h-24 w-full flex  items-center  tablet:mr-10 laptop:ml-[200px] tablet1004:mr-96 2.5xl:ml-2 2.7xl:ml-2 3xl:ml-2 desktopmd:ml-2 lg:justify-end lg:w-96 2xl:justify-start 2xl:w-80 2xl:pl-0  ">
+    <div>
+      <div className="bg-slate-700 h-24 w-full flex items-center">
+        <div className="h-24 w-full flex  items-center tablet:w-[53%] laptop:w-[34%] laptopL:w-[22%] laptopL2:w-[23%]">
           <Image
-            className="h-full w-full mb-2 mobile2:h-16  mobile2:mr-11 mobile3:mr-24 desktop-minimizar:mr-80 tablet-mitad:mr-80 tablet:mr-96 desktopmd:ml-0 lg:h-16 lg:w-96 2xl:h-20 2xl:w-full"
+            className="h-full w-[200px] ml-2.5 mb-1 mobile2:h-full mobile2:w-[210px]  mobile2:mb-1 mobile2:ml-2 mobile3:w-[215px]"
             src="../GameOnLogo.svg"
             width={1000}
             height={1000}
             alt="Logo de GameOn"
           />
         </div>
-        <div className="w-40 h-24 flex justify-end space-x-3 mr-5 items-center mobile2:mr-5 lg:w-full 2xl:space-x-3 2xl:mr-5">
-        <div className="2xl:flex 2xl:items-center 2xl:space-x-1 mt-2">
-            <p className="hidden 2xl:block 2xl:text-orange-300 2xl:font-semibold 2xl:cursor-pointer 2xl:text-md">Iniciar Sesión</p>
+        <div className="hidden tablet:flex tablet:justify-center tablet:items-center tablet:w-full tablet:h-12">
+        <input
+          className="w-[94%] h-10 rounded-full pl-5 font-bold text-md placeholder-gray-300 shadow-md items-center mb-8 mobile2:w-[95%] mobile3:w-[96%] 
+          tablet:mb-0 tablet:w-[100%]"
+          type="text"
+          placeholder="Ingrese su búsqueda"
+        />
+        </div>
+        <div className="w-40 h-24 flex justify-end space-x-1.5 mr-5 items-center mobile3:space-x-2 tablet:w-[18%] laptop:w-[12%] laptopL:w-[8%]
+        laptopL2:w-[24%]">
+          <div className="laptopL2:flex laptopL2:items-center laptopL2:space-x-1 mt-2">
+            <p className="hidden laptopL2:block laptopL2:text-orange-300 laptopL2:font-semibold ">
+              Iniciar Sesión
+            </p>
             <Image
-              className="h-8 w-8 cursor-pointer tablet:h-10 tablet:w-10 2xl:h-10 2xl:w-10"
+              className="h-[35px] w-[35px] cursor-pointer mt-0.5  mobile2:h-[36px] mobile2:w-[36px] mobile3:h-[36px] mobile3:w-[36px]"
               src="/User.png"
               width={500}
               height={500}
               alt="Usuario"
             />
           </div>
-        <div className=" 2xl:flex 2xl:items-center 2xl:space-x-1.5 mt-2">
-            <p className="hidden 2xl:block 2xl:text-orange-300 2xl:font-semibold 2xl:cursor-pointer 2xl:text-md">Carrito</p>
+          <div className="laptopL2:flex laptopL2:items-center laptopL2:space-x-1.5 ">
+            <p className="hidden laptopL2:block laptopL2:text-orange-300 laptopL2:font-semibold laptopL2:cursor-pointer laptopL2:text-md">
+              Carrito
+            </p>
             <Image
-            className="h-6 w-6 tablet:h-8 tablet:w-8 laptop:h-8 laptop:w-8 cursor-pointer"
-            src="/CarritoDeCompras.png"
-            width={1000}
-            height={1000}
-            alt="Carrito de Compras"
-          />
+              className="h-[27px] w-[27px] mt-2.5 cursor-pointer mobile2:h-[28px] mobile2:w-[28px] mobile3:h-[28px] mobile3:w-[28px]"
+              src="/CarritoDeCompras.png"
+              width={1000}
+              height={1000}
+              alt="Carrito de Compras"
+            />
           </div>
         </div>
+      </div>
       </div>
     </>
   );
