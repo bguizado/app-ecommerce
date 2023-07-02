ffe4e12ab878bccdf83be88736c5ce6120a30725
stefano <szevallosl@hotmail.com>
2023-07-02 12:14:51 -0500

    Header con cambios 02062023 , para hacer pull de la nueva API

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     <>
       <div>
         <div className="bg-slate-700 h-24 w-full flex items-center">
-          <div className="h-24 w-full flex  items-center tablet:w-[53%] laptop:w-[34%] laptopL:w-[22%] laptopL2:w-[23%] ">
+          <div className="h-24 w-full flex  items-center tablet:w-[53%] laptop:w-[34%] laptopL:w-[22%] laptopL2:w-[18%] ">
             <Link href={"./"} legacyBehavior>
               <a>
                 <Image
@@ -30,35 +30,26 @@ const Header = () => {
             />
           </div>
           <div
-            className="w-40 h-24 flex justify-evenly space-x-1.5 mr-5 items-center mobile3:space-x-2 tablet:w-[18%] laptop:w-[12%] laptopL:w-[8%]
-        laptopL2:w-[24%]"
+            className="w-40 h-24 flex justify-end mr-4 space-x-2.5 items-center mobile3:space-x-2 tablet:w-[18%] laptop:w-[12%] laptopL:w-[8%]
+        laptopL2:w-[8%] laptopL2:space-x-4"
           >
             <Link href={"/login"}>
-              <div className="laptopL2:flex laptopL2:items-center laptopL2:space-x-1 mt-2">
                 <Image
-                  className="h-[35px] w-[35px] cursor-pointer mt-0.5  mobile2:h-[36px] mobile2:w-[36px] mobile3:h-[36px] mobile3:w-[36px]"
+                  className="h-[35px] w-[35px] cursor-pointer mt-[11px]  mobile2:h-[36px] mobile2:w-[36px] mobile3:h-[36px] mobile3:w-[36px] laptopL2:h-[45px] laptopL2:w-[45px]"
                   src="/User.png"
                   width={500}
                   height={500}
                   alt="Usuario"
                 />
-              </div>
             </Link>
-            <div className="laptopL2:flex laptopL2:items-center laptopL2:space-x-1.5 ">
               <Image
-                className="h-[27px] w-[27px] mt-2.5 cursor-pointer mobile2:h-[28px] mobile2:w-[28px] mobile3:h-[28px] mobile3:w-[28px]"
+                className="h-[27px] w-[27px] mt-2.5 cursor-pointer mobile2:h-[28px] mobile2:w-[28px] mobile3:h-[28px] mobile3:w-[28px] laptopL2:h-[35px] laptopL2:w-[35px]"
                 src="/CarritoDeCompras.png"
                 width={1000}
                 height={1000}
                 alt="Carrito de Compras"
               />
-            </div>
-          </div>
-          <div
-            className="w-40 h-24 flex justify-end space-x-1.5 mr-5 items-center mobile3:space-x-2 tablet:w-[18%] laptop:w-[12%] laptopL:w-[8%]
-        laptopL2:w-[24%]"
-          >
-           
+ 
           </div>
           
         </div>
