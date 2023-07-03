3902708f83b19af4319ddbff042d9b1b9b1f7784
Arturo Bermudez <andherson.24@gmail.com>
2023-07-03 02:20:46 -0500

    funcion de login y registro usando local storage

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,75 @@
+"use client"
+import Link from "next/link";
+import React, { useState } from "react";
+import { json } from "stream/consumers";
+import { useRouter } from 'next/navigation'
+
+
+const LoginForm = () => {
+    const router = useRouter()
+
+    const [inputUsuario, setInputUsuario] = useState({
+        email: "",
+        password: "",
+    });
+
+    const handleLogin = (e:any) =>{
+        e.preventDefault();
+        const usuarioLogeado = JSON.parse(localStorage.getItem('usuario')!);
+
+        if(inputUsuario.email === usuarioLogeado.email &&
+            inputUsuario.password === inputUsuario.password){
+                router.push('/')
+            }
+            else{
+                alert("Correo o contraseña incorrecto")
+            }
+    }
+
+  return (
+    <div className="min-h-full flex items-center justify-center mt-30 py-12 sm:px-6 lg:px-8 text-gray-500">
+    <div>
+    <h1 className="mt-4 mb-20 text-center font-bold text-3xl">Iniciar Sesión</h1>
+    <form onSubmit={handleLogin}>
+    <div>
+        <div>
+            <input 
+            type="email"
+            name="email"
+            value={inputUsuario.email}
+            onChange={(e:any)=>
+                setInputUsuario({
+                    ...inputUsuario,
+                    [e.target.name]: e.target.value
+                })
+            }
+            className="w-full max-w-xs appearence-none px-3 py-2 border-2 rounded mb-10" 
+            placeholder="E-mail o Celular"/>
+        </div>
+        <div>
+            <input 
+            type="password" 
+            name="password"
+            value={inputUsuario.password}
+            onChange={(e:any)=>
+                setInputUsuario({
+                    ...inputUsuario,
+                    [e.target.name]: e.target.value
+                })
+            }
+            className="w-full max-w-xs appearence-none px-3 py-2 border-2 rounded mb-10" 
+            placeholder="Contraseña"/>
+        </div>
+        <div className="flex items-center justify-center">
+            <button className="bg-orange-400 text-white rounded-2xl py-1 px-6 mt-6 mb-4">Iniciar Sesión</button>
+        </div>
+    </div>
+</form>
+    <p className="mt-6 text-center text-sm">¿Olvidaste tu contraseña?</p>
+    <p className="mt-6 text-center text-sm">¿No tienes una cuenta? <Link className="text-orange-400 underline" href="/register">Registrate</Link></p>
+    </div>
+</div>
+  );
+};
+
+export default LoginForm;
