d4a43674935eb8cec8b3d7fac703cdf3ed1ce5c4
Edu Montalvo <kaedu18@gmail.com>
2023-07-06 13:18:58 -0500

    Finalizado y aplicado el componente modal a Producto

---
+++ src/components/Modal.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+const Modal = ({ isVisible, onClose, children }: { isVisible: boolean, onClose: () => void, children: React.ReactNode }) => {
+
+    if (!isVisible) return null;
+
+    return (
+        <>
+            <div className="w-full h-full flex justify-center items-center fixed top-0 left-0 bg-black bg-opacity-50 backdrop-blur">
+                <div className='w-80 h-1/2 bg-white relative rounded-xl p-5'>
+                    <div className='flex h-full flex-col justify-center items-center'>
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Modal
+
